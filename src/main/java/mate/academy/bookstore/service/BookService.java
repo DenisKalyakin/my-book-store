5f94a3b28f97a4741700efd656c76721817633fa
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-09-30 23:52:05 +0300

    Pagination and Swagger were added. Added saveAll() method for convenience

--- src/main/java/mate/academy/bookstore/service/BookService.java
+++ src/main/java/mate/academy/bookstore/service/BookService.java
@@ -4,17 +4,20 @@ import java.util.List;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.BookSearchParameters;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
     BookDto findById(Long id);
 
-    List<BookDto> getAll();
+    List<BookDto> getAll(Pageable pageable);
 
     void deleteById(Long id);
 
     BookDto update(BookDto bookDto, Long id);
 
-    List<BookDto> search(BookSearchParameters parameters);
+    List<BookDto> search(BookSearchParameters parameters, Pageable pageable);
+
+    String saveAll(CreateBookRequestDto[] requestDtos);
 }
