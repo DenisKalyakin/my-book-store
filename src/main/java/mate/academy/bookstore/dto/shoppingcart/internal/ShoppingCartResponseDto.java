7081b908d5efd42b198b7e3e74b3cf69322525e9
Denis Kalyakin <denis.kalyakin999@gmail.com>
2023-12-17 02:12:41 +0200

    Tests for the shopping cart functionality were added.

--- src/main/java/mate/academy/bookstore/dto/shoppingcart/internal/ShoppingCartResponseDto.java
+++ src/main/java/mate/academy/bookstore/dto/shoppingcart/internal/ShoppingCartResponseDto.java
@@ -2,9 +2,11 @@ package mate.academy.bookstore.dto.shoppingcart.internal;
 
 import java.util.Set;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import mate.academy.bookstore.dto.cartitem.internal.CartItemResponseDto;
 
 @Data
+@Accessors(chain = true)
 public class ShoppingCartResponseDto {
     private Long id;
     private Long userId;
